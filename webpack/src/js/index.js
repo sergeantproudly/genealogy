7d89ae1a42f103437c8e748e62d7ca4f068ac692
romanproudly <roman@proudly.ru>
2020-11-20 17:00:19 +0300

    full update

--- webpack/src/js/index.js
+++ webpack/src/js/index.js
@@ -21,7 +21,41 @@ import "../sass/main.sass";
 
 
 // START DEMO
-
+let registrationForm = document.querySelector(".login-and-registration__form__bottom_registration")
+if (registrationForm) {
+  let lastNameInput = registrationForm.querySelector('[name="lastname"]')
+  let sendForm = registrationForm.querySelector('.default-button')
+  lastNameInput.addEventListener('input', (event)=>{
+    if (lastNameInput.value != "") {
+      sendForm.removeAttribute('disabled')
+    } else {
+      sendForm.setAttribute('disabled', "")
+    }
+  })
+  registrationForm.addEventListener('submit', (event)=>{
+    event.preventDefault();
+    lastNameInput.parentNode.classList.add('default-field_error');
+    sendForm.classList.add('default-button_loader');
+  })
+  
+  let loginForm = document.querySelector(".login-and-registration__form__bottom_login")
+  let mailInput = loginForm.querySelector('[name="mail"]')
+  let passwordInput = loginForm.querySelector('[name="password"]')
+  let sendFormReg = loginForm.querySelector('.default-button')
+  mailInput.addEventListener('input', (event)=>{
+    if (mailInput.value != "") {
+      sendFormReg.removeAttribute('disabled')
+    } else {
+      sendFormReg.setAttribute('disabled', "")
+    }
+  })
+  loginForm.addEventListener('submit', (event)=>{
+    event.preventDefault();
+    mailInput.parentNode.parentNode.classList.add('field-with-icon_error');
+    passwordInput.parentNode.parentNode.classList.add('field-with-icon_error');
+    sendFormReg.classList.add('default-button_loader');
+  })
+}
 // END DEMO
 
 // временная постраничная навигация
@@ -54,6 +88,27 @@ window.closeLanguageDropdown = function() {
   languageShell.classList.remove('header__language_active-2')
   setTimeout(() => {
     languageShell.classList.remove('header__language_active-1')
+  }, 300);
+}
+
+let profileShell = document.querySelector('.header__profile')
+
+window.openProfileDropdown = function() {
+  if (profileShell.classList.contains('header__profile_active-2')) {
+    closeProfileDropdown()
+  } else {
+    profileShell.classList.add('header__profile_active-1')
+    setTimeout(() => {
+      profileShell.classList.add('header__profile_active-2')
+    }, 300);
+
+  }
+}
+
+window.closeProfileDropdown = function() {
+  profileShell.classList.remove('header__profile_active-2')
+  setTimeout(() => {
+    profileShell.classList.remove('header__profile_active-1')
   }, 300);
 }
 
@@ -93,3 +148,64 @@ window.turnLoginRegistration = function(name) {
   }
 }
 
+window.addEventListener("load", function(event) {
+  let dopFieldsButton = document.querySelector('.landing-order__form__dop')
+  let dopFields = document.querySelector('.landing-order__dop-fields')
+  let heightDopFields = dopFields.offsetHeight;
+  dopFields.style.height = `0px`
+
+  window.onToggleDopFields = function() {
+    if (dopFieldsButton.classList.contains('landing-order__form__dop_active')) {
+      dopFieldsButton.classList.remove('landing-order__form__dop_active')
+      dopFields.classList.remove('landing-order__dop-fields_active')
+      dopFields.style.height = `${0}px`
+    } else {
+      dopFieldsButton.classList.add('landing-order__form__dop_active')
+      dopFields.classList.add('landing-order__dop-fields_active')
+      dopFields.style.height = `${heightDopFields}px`
+
+    }
+    // handle event
+    window.addEventListener("optimizedResize", function() {
+      dopFields.style.height = `auto`
+      heightDopFields = dopFields.offsetHeight;
+      if (!dopFieldsButton.classList.contains('landing-order__form__dop_active')) {
+        dopFields.style.height = `0px`
+        return;
+      }
+      dopFields.style.height = `${heightDopFields}px`
+    });
+  }
+
+  let questions = document.querySelectorAll('.landing-fag__question');
+  function questionsInit() {
+    for (let index = 0; index < questions.length; index++) {
+      const question = questions[index];
+  
+      let answer = question.querySelector('.landing-fag__question__answer');
+      let heightAnswer = answer.offsetHeight;
+      answer.style.height = `0px`
+      
+      question.addEventListener('click', ()=>{
+        if (question.classList.contains('landing-fag__question_hidden')) {
+          answer.style.height = `0px`
+          return question.classList.remove("landing-fag__question_hidden");
+        }
+        answer.style.height = `${heightAnswer}px`
+        question.classList.add("landing-fag__question_hidden")
+      })
+
+      // handle event
+      window.addEventListener("optimizedResize", function() {
+        answer.style.height = `auto`
+        heightAnswer = answer.offsetHeight;
+        if (!question.classList.contains('landing-fag__question_hidden')) {
+          answer.style.height = `0px`
+          return;
+        }
+        answer.style.height = `${heightAnswer}px`
+      });
+    }
+  }
+  questionsInit()
+})
